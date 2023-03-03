ebc998c9d5534c251a90a5131064ebf44fcbdf65
lohithsrk <srklohith05@gmail.com>
2023-03-03 14:13:00 +0530

    cahnegs

--- database/database.js
+++ database/database.js
@@ -3,8 +3,8 @@ const Sequelize = require('sequelize');
 
 const db = {}
 
-const sequelize = new Sequelize.Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
-    host: process.env.HOST,
+const sequelize = new Sequelize.Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: 'postgres',
     logging: false
 })
